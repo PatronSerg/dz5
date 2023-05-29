c519fa4736db24b03715e571ed3110ae2d992d85
Serg <patron.serg@yandex.ru>
2023-05-29 13:02:03 +0300

    f

---
+++ task36/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int[] myArray = new int[10];
+Random random = new Random();
+for (int i = 0; i < myArray.Length; i++)
+{
+    myArray[i] = random.Next(1, 100);
+}
+int sum = 0;
+for (int i = 1; i < myArray.Length; i += 2)
+{
+    sum += myArray[i];
+}
+Console.WriteLine("Сумма элементов на нечетных позициях: " + sum);
---
+++ task38/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+
+
+double[] numbers = { 1.2, 3.4, 5.6, 7.8, 9.0 };
+double max = numbers[0];
+double min = numbers[0];
+foreach (double number in numbers)
+{
+    if (number > max)
+    {
+        max = number;
+    }
+    if (number < min)
+    {
+        min = number;
+    }
+}
+double difference = max - min;
+Console.WriteLine("Разница между максимальным и минимальным элементами: " + difference);
+
+
+
+
